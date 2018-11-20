2071290b8d5c244a1e4f2bcc71171c407115912b
lastrites2018 <lastrites@nate.com>
2018-11-20 14:39:59 +0900

    Makes blocks can break with keyboard input

--- src/game/App.js
+++ src/game/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./App.css";
-import Block from "./components/block";
-import Status from "./components/status";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import Block from './components/block';
+import Status from './components/status';
 
 const defaultState = () => {
   // check boardSize based on user's screen size
@@ -17,11 +17,11 @@ const defaultState = () => {
     // TODO: block objects are hardcoded below
     // generate default blocks dynamically
     blocks: [
-      { color: "red", key: "d" },
-      { color: "yellow", key: "a" },
-      { color: "blue", key: "s" },
-      { color: "yellow", key: "a" },
-      { color: "blue", key: "s" }
+      { color: 'red', key: 'd' },
+      { color: 'yellow', key: 'a' },
+      { color: 'blue', key: 's' },
+      { color: 'yellow', key: 'a' },
+      { color: 'blue', key: 's' }
     ]
   };
 };
@@ -32,9 +32,16 @@ class App extends React.Component {
     this.state = defaultState();
   }
 
-  _handleKeyDown(e) {
+  _handleKeyDown = e => {
     console.log(e.key);
-  }
+    // key 값이 일치하면, blocks 데이터를 삭제한다.
+    let nextBlocks = this.state.blocks.slice(0, this.state.blocks.length);
+    if (e.key === nextBlocks[nextBlocks.length - 1].key) {
+      nextBlocks.pop();
+      console.log('key가 일치하네요!');
+      this.setState({ blocks: nextBlocks });
+    }
+  };
 
   _generateDefaultBlocks() {
     return this.state.blocks.map((block, index) => {
