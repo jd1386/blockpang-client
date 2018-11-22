753a7940f4d0259e46dabb94a7af4b8c0ab6ce10
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-22 19:57:11 +0900

    fix class name

--- src/game/components/block/index.js
+++ src/game/components/block/index.js
@@ -8,7 +8,7 @@ class Block extends Component {
   render() {
     return (
       <div
-        className={`block ${this.props.index === 0 ? 'bottom' : null}`}
+        className={`block ${this.props.index === 0 ? 'block-bottom' : null}`}
         style={{ background: this.props.color }}
       >
         <BlockContent data={this.props} />
