50545e20645aff3124e11b3f4d887abcf8356c40
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-24 19:02:20 +0900

    finish separating Timer component

--- src/game/components/status/header.js
+++ src/game/components/status/header.js
@@ -2,23 +2,14 @@ import React, { Component } from 'react';
 import './style.scss';
 import { Spring } from 'react-spring';
 import { Image } from 'semantic-ui-react';
-class Header extends Component {
-  formatUnitOfTime(unitOfTime) {
-    return unitOfTime < 10
-      ? `0${unitOfTime}`.substring(0, 2)
-      : unitOfTime.toString().substring(0, 2);
-  }
-  render() {
-    let displayTime;
-    const seconds = this.formatUnitOfTime(Math.floor(this.props.time / 1000));
-    const milliseconds = this.formatUnitOfTime(this.props.time % 1000);
-    displayTime = `${seconds}:${milliseconds}`;
+import Timer from './timer';
 
+class Header extends Component {
+  render() {
     return (
       <div className="game-status-bar">
         <div className="game-status-component">
-          <Image id="onlycoin" size="mini" src="clock.gif" />
-          <span className="status-title">{displayTime}</span>
+          <Timer time={this.props.time} />
         </div>
         <Spring
           from={{ number: this.props.currentScore }}
