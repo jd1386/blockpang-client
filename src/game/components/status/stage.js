ce2d36bf46fccb63ddb59948744f4ae4db2e2cc8
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-27 17:19:50 +0900

    change icons to stage

--- src/game/components/status/stage.js
+++ src/game/components/status/stage.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
+import { Spring } from 'react-spring';
 import PropTypes from 'prop-types';
 
 const stage = props => (
   <React.Fragment>
     <Image id="onlycoin" size="mini" src="icon.png" style={{ width: '4vw' }} />
-    <span className="status-title">X {props.icons}</span>
+    <span className="status-title">X {props.stage}</span>
   </React.Fragment>
 );
 
 stage.propTypes = {
-  icons: PropTypes.number.isRequired
+  stage: PropTypes.number.isRequired
 };
 
 export default stage;
