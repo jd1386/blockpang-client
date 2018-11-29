7abef63a39234e60a47697e8e826f6edeed8bde4
Jungdo Lee <lee.jungdo@gmail.com>
2018-11-29 16:18:40 +0900

    add more blank layout

--- src/components/admin/dashboard.js
+++ src/components/admin/dashboard.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container, Grid, Header, Button, Segment } from 'semantic-ui-react';
+import {
+  Container,
+  Grid,
+  Header,
+  Button,
+  Image,
+  Breadcrumb,
+  Statistic,
+  Segment
+} from 'semantic-ui-react';
 
 class Dashboard extends Component {
   render() {
@@ -9,30 +18,68 @@ class Dashboard extends Component {
       <Segment vertical>
         <Grid container stackable verticalAlign="middle">
           <Grid.Row>
-            <Header as="h1">Dashboard</Header>
+            <Grid.Column width={13}>
+              <Header as="h1">Dashboard</Header>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Breadcrumb>
+                <Breadcrumb.Section>Admin</Breadcrumb.Section>
+                <Breadcrumb.Divider icon="right angle" />
+                <Breadcrumb.Section active>
+                  <a href="/admin">Dashboard</a>
+                </Breadcrumb.Section>
+              </Breadcrumb>
+            </Grid.Column>
           </Grid.Row>
+          {/* Summary */}
           <Grid.Row>
-            <Grid.Column>
-              <Header as="h3" style={{ fontSize: '2em' }}>
-                We Help Companies and Companions
-              </Header>
-              <p style={{ fontSize: '1.33em' }}>
-                We can give your company superpowers to do things that they
-                never thought possible. Let us delight your customers and
-                empower your needs... through pure data analytics.
-              </p>
-              <Header as="h3" style={{ fontSize: '2em' }}>
-                We Make Bananas That Can Dance
-              </Header>
-              <p style={{ fontSize: '1.33em' }}>
-                Yes that's right, you thought it was the stuff of dreams, but
-                even bananas can be bioengineered.
-              </p>
+            <Grid.Column width={4}>
+              <Segment padded textAlign="center">
+                <Statistic>
+                  <Statistic.Value>64,104</Statistic.Value>
+                  <Statistic.Label>Users</Statistic.Label>
+                </Statistic>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Segment padded textAlign="center">
+                <Statistic>
+                  <Statistic.Value>390,110</Statistic.Value>
+                  <Statistic.Label>Plays</Statistic.Label>
+                </Statistic>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Segment padded textAlign="center">
+                <Statistic>
+                  <Statistic.Value>596,023</Statistic.Value>
+                  <Statistic.Label>Coins Paid</Statistic.Label>
+                </Statistic>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <Segment padded textAlign="center">
+                <Statistic>
+                  <Statistic.Value>596,023</Statistic.Value>
+                  <Statistic.Label>Coins Paid</Statistic.Label>
+                </Statistic>
+              </Segment>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-            <Grid.Column textAlign="center">
-              <Button size="huge">Check Them Out</Button>
+            <Grid.Column width={8}>
+              <Segment padded>Content here</Segment>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Segment padded>Content here</Segment>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Segment padded>Content here</Segment>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Segment padded>Content here</Segment>
             </Grid.Column>
           </Grid.Row>
         </Grid>
