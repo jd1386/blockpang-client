d635232556b18a7057133c12fd8de0a3ac1983c3
Jungdo Lee <lee.jungdo@gmail.com>
2018-12-03 13:01:43 +0900

    split into separate methods

--- src/components/MyPage/gameRecord.js
+++ src/components/MyPage/gameRecord.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { takeRight, pick } from 'lodash';
-import { Table, Container, Grid, Button } from 'semantic-ui-react';
+import { Table, Container, Grid, Button, Loader } from 'semantic-ui-react';
 import {
   BarChart,
   Bar,
@@ -16,6 +16,69 @@ class GameRecord extends Component {
   state = {
     transactions: []
   };
+
+  _loader() {
+    return <Loader active inline="centered" content="Loading" />;
+  }
+
+  _renderTable() {
+    return (
+      <React.Fragment>
+        <Table basic="very" celled collapsing style={{ width: '100%' }}>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Timestamp</Table.HeaderCell>
+              <Table.HeaderCell>Score</Table.HeaderCell>
+              <Table.HeaderCell>ICX won</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {this.state.transactions.map((transaction, index) => {
+              return (
+                <Table.Row key={index}>
+                  <Table.Cell>{transaction.timestamp}</Table.Cell>
+                  <Table.Cell>{transaction.amount * 100}</Table.Cell>
+                  <Table.Cell>{transaction.amount}</Table.Cell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table>
+        {this._moreTransactionsBtn()}
+      </React.Fragment>
+    );
+  }
+
+  _renderGraph() {
+    return (
+      <BarChart width={400} height={400} data={this.state.transactions}>
+        <XAxis dataKey="timestamp" />
+        <YAxis />
+        <Tooltip />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Legend />
+        <Bar dataKey="amount" fill="#24c2d4f2" />
+      </BarChart>
+    );
+  }
+
+  _moreTransactionsBtn() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '2em' }}>
+        <a
+          href={`https://tracker.icon.foundation/address/${localStorage.getItem(
+            'walletAddress'
+          )}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Button primary>View more transactions</Button>
+        </a>
+      </div>
+    );
+  }
+
   componentDidMount() {
     // TODO: replace all transactions with transactions by user
     axios
@@ -44,51 +107,17 @@ class GameRecord extends Component {
               <div style={{ textAlign: 'center', marginBottom: '1em' }}>
                 <h2>Recent Transactions</h2>
               </div>
-              <Table basic="very" celled collapsing style={{ width: '100%' }}>
-                <Table.Header>
-                  <Table.Row>
-                    <Table.HeaderCell>Timestamp</Table.HeaderCell>
-                    <Table.HeaderCell>Score</Table.HeaderCell>
-                    <Table.HeaderCell>ICX won</Table.HeaderCell>
-                  </Table.Row>
-                </Table.Header>
-
-                <Table.Body>
-                  {this.state.transactions.map((transaction, index) => {
-                    return (
-                      <Table.Row key={index}>
-                        <Table.Cell>{transaction.timestamp}</Table.Cell>
-                        <Table.Cell>{transaction.amount * 100}</Table.Cell>
-                        <Table.Cell>{transaction.amount}</Table.Cell>
-                      </Table.Row>
-                    );
-                  })}
-                </Table.Body>
-              </Table>
-              <div style={{ textAlign: 'center' }}>
-                <a
-                  href={`https://tracker.icon.foundation/address/${localStorage.getItem(
-                    'walletAddress'
-                  )}`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <Button primary>View more transactions</Button>
-                </a>
-              </div>
+              {this.state.transactions.length
+                ? this._renderTable()
+                : this._loader()}
             </Grid.Column>
             <Grid.Column>
               <div style={{ textAlign: 'center', marginBottom: '1em' }}>
                 <h2>ICX Payout by timeline</h2>
               </div>
-              <BarChart width={400} height={400} data={this.state.transactions}>
-                <XAxis dataKey="timestamp" />
-                <YAxis />
-                <Tooltip />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Legend />
-                <Bar dataKey="amount" fill="#24c2d4f2" />
-              </BarChart>
+              {this.state.transactions.length
+                ? this._renderGraph()
+                : this._loader()}
             </Grid.Column>
           </Grid.Row>
         </Grid>
