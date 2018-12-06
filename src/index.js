d81fa8eeb7487c7ef0ef277fda0f5b1cdf20c8fc
Jungdo Lee <lee.jungdo@gmail.com>
2018-12-06 17:23:11 +0900

    disable serviceworker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
+import { unregister } from './serviceWorker';
 // import WebFont from 'webfontloader';
 
 // WebFont.load({
@@ -17,4 +18,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+unregister();
