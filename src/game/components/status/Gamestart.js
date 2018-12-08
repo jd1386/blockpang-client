a5806913164b5eb13e75aea489112dc07171dfee
Jungdo Lee <lee.jungdo@gmail.com>
2018-12-08 13:08:32 +0900

    finish basic button layout and styling

--- src/game/components/status/Gamestart.js
+++ src/game/components/status/Gamestart.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './style.scss';
 import PropTypes from 'prop-types';
+import { Button, Icon } from 'semantic-ui-react';
 
 const Gamestart = props => {
   return (
-    <div className="game-status-main" onClick={props.onClick()}>
-      <div className="header">
-        Block
-        <br /> Pang
+    <div className="game-status-main">
+      <div>
+        <Button color="black">
+          <div className="eight-bit-btn">
+            Get 20 ICX without Play{' '}
+            <Icon name="heart" color="red" style={{ fontSize: '1.5em' }} />
+          </div>
+        </Button>
       </div>
-      <div className="content">
-        <div>Click to Play</div>
-        <div className="flash">Insert (1) Coin ...</div>
+      <div onClick={props.onClick()}>
+        <div className="header">
+          Block
+          <br /> Pang
+        </div>
+        <div className="content">
+          <div>Click to Play</div>
+          <div className="flash">Insert (1) Coin ...</div>
+        </div>
       </div>
     </div>
   );
