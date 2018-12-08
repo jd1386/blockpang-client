7b3283518d7316474cc1526d0921052ab9ba8169
Jungdo Lee <lee.jungdo@gmail.com>
2018-12-08 11:28:11 +0900

    make sticky footer and other styling

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -5,18 +5,15 @@ import MainPage from '../components/Main';
 class Main extends Component {
   render() {
     return (
-      <div>
-        <Segment style={{ padding: '5.5em 0em' }} vertical>
-          <Grid container stackable verticalAlign="middle">
-            <Grid.Row>
-              <Grid.Column>
-                <MainPage.Board isPlayingGame={this.props.isPlayingGame} />
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row />
-          </Grid>
-        </Segment>
-      </div>
+      <Segment style={{ paddingTop: '11vh' }} vertical>
+        <Grid container stackable verticalAlign="middle">
+          <Grid.Row>
+            <Grid.Column>
+              <MainPage.Board isPlayingGame={this.props.isPlayingGame} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
     );
   }
 }
