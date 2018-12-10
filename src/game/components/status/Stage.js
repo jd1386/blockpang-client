9eb14fe460a662a03f7ce3bd3f44e03d2b9c5554
Jungdo Lee <lee.jungdo@gmail.com>
2018-12-10 11:48:55 +0900

    remove stage image

--- src/game/components/status/Stage.js
+++ src/game/components/status/Stage.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { Image } from 'semantic-ui-react';
-import iconImage from '../../assets/img/icon.png';
 import PropTypes from 'prop-types';
 
 class Stage extends React.PureComponent {
   render() {
-    return (
-      <React.Fragment>
-        <Image
-          id="onlycoin"
-          size="mini"
-          src={iconImage}
-          style={{ width: '4vw' }}
-        />
-        <span className="status-title">X {this.props.stage}</span>
-      </React.Fragment>
-    );
+    return <span className="status-title">Stage {this.props.stage}</span>;
   }
 }
 
